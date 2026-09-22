c6c1bc56b79d5957fef21cf92410a7d7b2b5e771
Natik <natik.agaev.emkave@gmail.com>
2026-09-22 22:05:33 +0200

    begin

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    kotlin("jvm") version "2.4.20"
+    id("org.jetbrains.intellij.platform") version "2.19.0"
+}
+
+group = "org.vidbg"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    intellijPlatform {
+        intellijIdea("2026.2.3")
+        bundledPlugin("com.intellij.java")
+        testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
+    }
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        name = "ViDBG"
+        version = project.version.toString()
+        ideaVersion {
+            sinceBuild = "262"
+        }
+    }
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
